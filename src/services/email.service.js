f9e0a949eb04543e4d8f588c2f8c86c7918586de
nimurr <nimurnerob404@gmail.com>
2025-12-09 11:29:30 +0600

    `Updated email templates in email.service.js`

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -28,14 +28,14 @@ const sendEmailVerification = async (to, otp) => {
   <body style="background-color: #f3f4f6; padding: 2rem; font-family: Arial, sans-serif; color: #333;">
     <div
         style="max-width: 32rem; margin: 0 auto; background-color: #ffffff; padding: 2rem; border-radius: 0.75rem; box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15); text-align: center;">
-        <img src="https://raw.githubusercontent.com/shadat-hossan/Image-server/refs/heads/main/NEXMOTAG.jpeg"
-            alt="NEXMO TAG" style="max-width: 10rem; margin-bottom: 1.5rem;">
-        <h1 style="font-size: 1.75rem; font-weight: 700; margin-bottom: 1rem; color: #1f2937;">Welcome to NEXMO TAG
-        </h1>
-        <p style="color: #4b5563; margin-bottom: 1.5rem;">Thank you for joining NEXMO TAG! Your account is almost
+         <h1 style="font-size: 3rem; font-weight: 700; margin-bottom: 1rem; color: #34d399;">RevoIV
+              </h1>
+              
+        <h1 style="font-size: 1.75rem; font-weight: 700; margin-bottom: 1rem; color: #1f2937;">Welcome to RevoIV</h1>
+        <p style="color: #4b5563; margin-bottom: 1.5rem;">Thank you for joining RevoIV TAG! Your account is almost
             ready.</p>
         <div
-            style="background: linear-gradient(135deg, #3b82f6, #06b6d4); color: #ffffff; padding: 1rem; border-radius: 0.5rem; font-size: 2rem; font-weight: 800; letter-spacing: 0.1rem; margin-bottom: 1.5rem;">
+            style="background: linear-gradient(135deg, #35e6a5ff, #22b37dff); color: #ffffff; padding: 1rem; border-radius: 0.5rem; font-size: 2rem; font-weight: 800; letter-spacing: 0.1rem; margin-bottom: 1.5rem;">
             ${otp}
         </div>
         <p style="color: #4b5563; margin-bottom: 1.5rem;">Collect this code to verify your account.</p>
@@ -57,14 +57,15 @@ const sendResetPasswordEmail = async (to, otp) => {
       <body style="background-color: #f3f4f6; padding: 2rem; font-family: Arial, sans-serif; color: #333;">
           <div
               style="max-width: 32rem; margin: 0 auto; background-color: #ffffff; padding: 2rem; border-radius: 0.75rem; box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15); text-align: center;">
-              <img src="https://raw.githubusercontent.com/shadat-hossan/Image-server/refs/heads/main/NEXMOTAG.jpeg"
-                  alt="NEXMO-TAG" style="max-width: 8rem; margin-bottom: 1.5rem;">
+          
+              <h1 style="font-size: 3rem; font-weight: 700; margin-bottom: 1rem; color: #34d399;">RevoIV
+              </h1>
               <h1 style="font-size: 1.75rem; font-weight: 700; margin-bottom: 1rem; color: #1f2937;">Password Reset Request
               </h1>
               <p style="color: #4b5563; margin-bottom: 1.5rem;">You requested a password reset for your account. Use the code
                   below to reset your password:</p>
               <div
-                  style="background: linear-gradient(135deg, #3d56ad, #0032D3); color: #ffffff; padding: 1rem; border-radius: 0.5rem; font-size: 2rem; font-weight: 800; letter-spacing: 0.1rem; margin-bottom: 1.5rem;">
+                  style="background: linear-gradient(135deg, #35e6a5ff, #22b37dff); color: #ffffff; padding: 1rem; border-radius: 0.5rem; font-size: 2rem; font-weight: 800; letter-spacing: 0.1rem; margin-bottom: 1.5rem;">
                   ${otp}
               </div>
               <p style="color: #d6471c; margin-bottom: 1.5rem;">Collect this code to reset your password. This code is valid
